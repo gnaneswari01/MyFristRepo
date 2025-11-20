f20932af4e2e9ffd97d7b0671ad991673166c422
gnaneswari01 <yadavgnane@gmail.com>
2025-11-20 18:58:00 +0530

    Update app.java

--- app.java
+++ app.java
@@ -12,14 +12,7 @@
   </html>
 
 
-  <!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-
-<h1>This is a heading</h1>
+  <!
 <p>This is a paragraph.</p>
 
 </body>
